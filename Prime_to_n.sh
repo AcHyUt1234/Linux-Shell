1edbbfeab80687b63350f732f9beb12ccfb923e2
Achyut Dubey <achyut.dubey6.4@gmail.com>
2020-11-08 22:11:00 +0530

    Find Prime numbers up to given 'N'
    Print Prime numbers up to given number.
    Sample Input: 5
    Output: 2 3 5

---
+++ Prime_to_n.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+num=$1
+n=$num
+
+if [ -z "$1" ]
+then
+echo "-1"
+else
+
+for((i=1;i<n;i++))
+do
+flag=0
+for((j=2;j<i;j++))
+do
+
+if [ $(expr "$i" % "$j") -eq 0 ]
+then
+flag=1
+fi
+done
+
+if [ $flag -eq 0 ]
+then
+echo "$i"
+fi
+done
+fi
